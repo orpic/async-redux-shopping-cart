9857e6b765908a6086fd6cd9b9ee42dd70fa3977
shobhit-u <shobhitumrao5@gmail.com>
2023-01-01 18:26:45 +0530

    notification container/component complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 
-import { Cart, Header, Item } from "./container";
+import { Cart, Header, Item, Notification } from "./container";
 import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisible);
   return (
     <>
+      <Notification />
       <Header />
       {showCart && <Cart />}
       <Item />
