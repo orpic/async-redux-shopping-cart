fd6e1860a6f37d1fe6ecfabe6b79230fe9c73231
shobhit-u <shobhitumrao5@gmail.com>
2023-01-01 22:56:20 +0530

    send data complete

--- src/App.js
+++ src/App.js
@@ -2,26 +2,73 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 import { Cart, Header, Item, Notification } from "./container";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/uiSlice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisible);
 
-  //can be done in any component
-  // having side effect logic in a component
-  //and the data tranformation logic inside a reducer
+  //
+  const notification = useSelector((state) => state.ui.notification);
+  //
+  const dispatch = useDispatch();
+  // side effect logic
+  // can be implemented in any component
+  // and the data tranformation logic inside a reducer
   const cart = useSelector((state) => state.cart);
   const databaseUrl = process.env.REACT_APP_realtimeDatabaseUrl + "/cart.json";
   useEffect(() => {
-    fetch(databaseUrl, {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending",
+        message: "Sending Cart Data",
+      })
+    );
+    const sendCartData = async () => {
+      const response = await fetch(databaseUrl, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong, cart data not sent");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Added to cart",
+        })
+      );
+
+      // const responseData = await response.json();
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Something went wrong",
+          message: "Card data not sent",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, databaseUrl, dispatch]);
+  // }, [cart, dispatch]);
+  // react-redux ensures that dispatch never changes
+  // can be added for completness
+  // console.log(notification);
 
   return (
     <>
-      <Notification />
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Header />
       {showCart && <Cart />}
       <Item />
