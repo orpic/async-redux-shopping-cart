cbe961b66e87558c1faf9cd3f9b97d2b0fb4761d
shobhit-u <shobhitumrao5@gmail.com>
2023-01-01 18:55:33 +0530

    send data complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import { Cart, Header, Item, Notification } from "./container";
@@ -6,6 +6,19 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisible);
+
+  //can be done in any component
+  // having side effect logic in a component
+  //and the data tranformation logic inside a reducer
+  const cart = useSelector((state) => state.cart);
+  const databaseUrl = process.env.REACT_APP_realtimeDatabaseUrl + "/cart.json";
+  useEffect(() => {
+    fetch(databaseUrl, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <>
       <Notification />
