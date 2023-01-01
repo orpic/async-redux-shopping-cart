54a7e82111918891bc64a2e096910d2811ad46c6
shobhit-u <shobhitumrao5@gmail.com>
2023-01-02 01:49:20 +0530

    cart fetch find on undefined bug fix

--- src/store/cart-Actions.js
+++ src/store/cart-Actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
